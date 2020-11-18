1319d05ba9565306c8478e7bb8c16c4aa1bcfcd5
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-11-18 21:36:34 +0100

    manca il bonus

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const myApp = new Vue({
+  el: "#root",
+  data: {
+    itemList: [],
+    inputItem: "",
+
+  },
+  methods: {
+    takeInput: function(){
+      this.itemList.push(this.inputItem)
+    },
+    removeItem: function(index){
+      this.itemList.splice(index, 1)
+    }
+  }
+})
